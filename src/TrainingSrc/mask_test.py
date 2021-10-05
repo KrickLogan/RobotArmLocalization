c79e7a2cbc6aa91ab6ce389f0d14bb2132a55ea3
Logan K <logankrick@gmail.com>
2021-10-05 13:33:47 -0500

    Update mask_test.py
    - outputs a list of masks that require rework
    - checks for incorrect colors in the mask

--- src/TrainingSrc/mask_test.py
+++ src/TrainingSrc/mask_test.py
@@ -4,12 +4,15 @@ from PIL import Image
 
 masks = list(sorted(os.listdir("Data/PNGMasks")))
 images = list(sorted(os.listdir("Data/PNGImages")))
-np_masks=[]
+np_masks = []
 np_imgs = []
+correct_unique_values = [0,127,255]
+rework_masks = []
+rework_masks_colors = []
 
 for img in images:
-
     np_imgs.append((np.array(Image.open(os.path.join("Data/PNGImages",img))), img))
+
 for mask in masks:
     np_masks.append((np.array(Image.open(os.path.join("Data/PNGMasks", mask))), mask))
 
@@ -27,5 +30,20 @@ for mask_d,img_d in zip(np_masks, np_imgs):
     print(f"mask shape = {mask.shape}, img shape = {img.shape} dimension equivalent: {dimension}")
     print(f"unique values mask data: {np.unique(mask)} {num_unique_values == 3}")
     
+    # Check for incorrect dimension or number of unique values
     if not dimension or num_unique_values != 3:
-        print(mask_name)
+        print(f'!!! Rework Required: {mask_name}')
+        rework_masks.append(mask_name)
+
+    # Check for incorrect colors
+    if not np.array_equal(np.unique(mask), correct_unique_values):
+        print(f'!!! Rework Required: {mask_name}')
+        rework_masks_colors.append(mask_name)
+
+# Display arrays of the masks that require rework
+print('\n--------------------------------------------------------------------------------------')
+print('\nThe following masks require rework:')
+print('\nRemove alpha channel or set to grayscale in the following masks:')
+print(rework_masks)
+print('\nIncorrect colors are used in the following masks:')
+print(rework_masks_colors)
