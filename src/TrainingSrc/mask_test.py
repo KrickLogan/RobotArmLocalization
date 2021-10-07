6beb79341b7505bdc4c1d3a8837ce2b7a714ea95
Logan K <logankrick@gmail.com>
2021-10-07 11:49:59 -0500

    Update mask_test.py
    - moved 'number of unique value check' to be included in 'incorrect color check' since they go hand in hand
    - updated output

--- src/TrainingSrc/mask_test.py
+++ src/TrainingSrc/mask_test.py
@@ -6,9 +6,9 @@ masks = list(sorted(os.listdir("Data/PNGMasks")))
 images = list(sorted(os.listdir("Data/PNGImages")))
 np_masks = []
 np_imgs = []
-correct_unique_values = [0,64,127,255]
 rework_masks = []
 rework_masks_colors = []
+correct_unique_values = [0,64,127,255]
 
 for img in images:
     np_imgs.append((np.array(Image.open(os.path.join("Data/PNGImages",img))), img))
@@ -23,20 +23,19 @@ for mask_d,img_d in zip(np_masks, np_imgs):
     mask_name = mask_d[1]
     img_name = img_d[1]
 
-    print('')
-    print(f'Mask: {mask_name}')
+    print(f'\nMask: {mask_name}')
     dimension = mask.shape == img.shape[:-1]
     num_unique_values = len(np.unique(mask))
     print(f"mask shape = {mask.shape}, img shape = {img.shape} dimension equivalent: {dimension}")
     print(f"unique values mask data: {np.unique(mask)} {num_unique_values == 4}")
     
-    # Check for incorrect dimension or number of unique values
-    if not dimension or num_unique_values != 4:
+    # Check for incorrect dimension (grayscale/alpha channel check)
+    if not dimension:
         print(f'!!! Rework Required: {mask_name}')
         rework_masks.append(mask_name)
 
-    # Check for incorrect colors
-    if not np.array_equal(np.unique(mask), correct_unique_values):
+    # Check for incorrect colors or number of unique values
+    if not np.array_equal(np.unique(mask), correct_unique_values) or num_unique_values != 4:
         print(f'!!! Rework Required: {mask_name}')
         rework_masks_colors.append(mask_name)
 
@@ -45,5 +44,5 @@ print('\n-----------------------------------------------------------------------
 print('\nThe following masks require rework:')
 print('\nRemove alpha channel or set to grayscale in the following masks:')
 print(rework_masks)
-print('\nIncorrect colors are used in the following masks:')
+print('\nIncorrect number of unique values or incorrect colors are used in the following masks:')
 print(rework_masks_colors)
