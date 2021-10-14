abe5756c047d877254cae3c2d400e9e69b15c253
Logan K <logankrick@gmail.com>
2021-10-14 17:03:42 -0500

    Update test_run_script.py
    - retrieves depth arrays from Data/Depths/
    - retrieves images from Data/Images/
    - updated titles for plots
    - function to calculate average depth of mask
    - display depth value at cursor location on show_mask_with_score_label_depth plt
    - run visualization on all images/depths in the Data folder
    - cleaned up unused code
    - no longer requires system args when running the script (uses data folder now)
    - prints Labels, Scores, and Avg_Depths for each image

--- src/RobotArmLocalizationPackage/test_run_script.py
+++ src/RobotArmLocalizationPackage/test_run_script.py
@@ -1,15 +1,28 @@
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 import matplotlib.image as mpimg
+from PIL import Image
 import numpy.ma as ma
 import numpy as np
 import numpy
 import torch
-import sys
-
+import os
 from torchvision.transforms import functional as F
 
 PRECISION = 0.75
+
+depths = list(sorted(os.listdir("src/RobotArmLocalizationPackage/Data/Depths")))
+images = list(sorted(os.listdir("src/RobotArmLocalizationPackage/Data/Images")))
+np_depths = []
+np_imgs = []
+
+for depth in depths:
+    if not os.path.basename(depth) == '.gitkeep':
+        np_depths.append(np.array(np.load(os.path.join("src/RobotArmLocalizationPackage/Data/Depths/", depth))))
+
+for img in images:
+    if not os.path.basename(img) == '.gitkeep':
+        np_imgs.append(Image.open(os.path.join("src/RobotArmLocalizationPackage/Data/Images/",img)))
 
 def load_model():
     model = torch.load('model.pt', map_location=torch.device('cpu'))
@@ -30,14 +43,35 @@ def get_bool_masks(masks):
 def get_mask_layer(bool_masks):
     composite_mask = numpy.zeros((bool_masks[0].size()), bool)
     for mask in bool_masks:
-        # print(mask.size())
         m = mask.numpy()
         composite_mask = numpy.logical_or(composite_mask, m)
     return composite_mask
-    
+
+def get_label_string(label):
+    label_string = ''
+    if label == 1:
+        label_string = 'Base'
+    elif label == 2:
+        label_string = 'Claw'
+    elif label == 3:
+        label_string = 'Cotton'
+    elif label == 0:
+        label_string = 'Background'
+    else:
+        label_string = 'Error!'
+    return label_string
+
+def show_img(img):
+    plt.imshow(img)
+    plt.title('Original Image')
+    maximize_plt()
+    plt.show()
+
 def show_img_mask(img, mask):
     plt.imshow(img)
     plt.imshow(mask, cmap='ocean', alpha=.5)
+    plt.title('Predicted Masks')
+    maximize_plt()
     plt.show()
 
 def show_all_masks(img, masks):
@@ -47,76 +81,85 @@ def show_all_masks(img, masks):
 
 def show_mask_with_depth(img, bmask, depth):
     plt.imshow(img)
-    plt.imshow(bmask.numpy(), cmap='ocean', alpha=.5)
+    plt.imshow(bmask.numpy(), cmap='ocean', alpha=.25)
     plt.title('Depth: ' + str(depth))
     plt.show()
 
 def show_mask_with_score_label(img, bmask, score, label):
     plt.imshow(img)
     plt.imshow(bmask.numpy(), cmap='ocean', alpha=.5)
-    plt.title(f'Score: {score}, Label: {label}')
+    plt.title(f'Label: {get_label_string(label)}, Score: {round(score.item(),4)}')
+    maximize_plt()
     plt.show()
 
-def get_img(fname):
-    img_path = fname
-    img = mpimg.imread(img_path)
-    return img
+def show_mask_with_score_label_depth(img, bmask, score, label, depth, depth_arr):
+    plt.imshow(img)
+    plt.imshow(bmask.numpy(), cmap='ocean', alpha=.5)
+    plt.imshow(depth_arr, alpha=0)
+    plt.title(f'Label: {get_label_string(label)}, Score: {round(score.item(),4)}, Average Depth: {round(depth, 4)}')
+    maximize_plt()
+    plt.show()
 
 def show_img_boxes(img, rectangles):
     plt.imshow(img)
+    plt.title('Predicted Bounding Boxes')
     for rect in rectangles:
         plt.gca().add_patch(rect)
 
+    maximize_plt()
     plt.show()
 
-def get_depth_of_seg(depth_arr, bool_seg_mask):
+def get_avg_depth_of_seg(depth_arr, bool_seg_mask):
     mx = ma.masked_array(depth_arr, np.invert(bool_seg_mask).long())
-    print(mx)
     return mx.mean()
-
-def get_rand_depth_array(size):
-    return numpy.random.randint(0, 500, size) 
 
 def size_img_tensor(tens):
     if tens.size()[0] > 3:
         tens = tens[0:3]
     return tens
 
-def main(file_name):
-    model = load_model()
-    img = get_img(file_name)
-    img_tens = F.to_tensor(img)
-    img_tens = size_img_tensor(img_tens)
-    output = model([img_tens])
-    
-    boxes = output[0]['boxes']
-    labels = output[0]['labels']
-    masks = output[0]['masks']
-    scores = output[0]['scores']
+def maximize_plt():
+    # if OS is Windows the plot will be maximized :)
+    if os.name == 'nt':
+        mng = plt.get_current_fig_manager() 
+        mng.window.state("zoomed")
 
-    rectangles = get_rectangles(boxes)
-    show_img_boxes(img, rectangles)
+def main():
+    for i in range(len(np_imgs)):
+        img = np_imgs[i]
+        depth_arr = np_depths[i]
 
-    show_all_masks(img, masks)
+        model = load_model()
+        img_tens = F.to_tensor(img)
+        img_tens = size_img_tensor(img_tens)
+        output = model([img_tens])
+        
+        boxes = output[0]['boxes']
+        labels = output[0]['labels']
+        masks = output[0]['masks']
+        scores = output[0]['scores']
 
-    size = img_tens[0].size()
-    depth_arr = get_rand_depth_array(size)
-    avg_depths = []
-    bool_masks = get_bool_masks(masks)
-    for bmask in bool_masks: 
-        avg_depths.append(get_depth_of_seg(depth_arr, bmask))
+        show_img(img)
 
-    print(labels)
-    print(scores)
+        rectangles = get_rectangles(boxes)
+        show_img_boxes(img, rectangles)
 
-    for i in range(len(masks)):
-        if scores[i] >= 0.90:
-            show_mask_with_score_label(img, bool_masks[i], scores[i], labels[i])
+        show_all_masks(img, masks)
 
-    # for bmask, depth in zip(bool_masks, avg_depths):
-    #     show_mask_with_depth(img, bmask, depth) 
+        avg_depths = []
+        bool_masks = get_bool_masks(masks)
+        for bmask in bool_masks: 
+            avg_depths.append(get_avg_depth_of_seg(depth_arr, bmask))
+
+        print(f'Labels: {labels}')
+        print(f'Scores: {scores}')
+        print(f'Avg Depths: {avg_depths}')
+
+        for i in range(len(masks)):
+            #if scores[i] >= 0.90:
+            show_mask_with_score_label_depth(img, bool_masks[i], scores[i], labels[i], avg_depths[i], depth_arr)
+
 
 if __name__ == "__main__":
-    file_name = sys.argv[1]
-    main(file_name)
+    main()
     
