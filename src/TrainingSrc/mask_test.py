404d1bce2d4ca40810d4860035d7e63e1be54890
tdrps <70676631+tdrps@users.noreply.github.com>
2021-10-15 15:07:11 -0500

    functions within mask test for getting and checking file names based on mask filenames

--- src/TrainingSrc/mask_test.py
+++ src/TrainingSrc/mask_test.py
@@ -1,30 +1,40 @@
 import os
 import numpy as np
 from PIL import Image
+import pytest
 
-def get_filename_prefixes(mask_file_names):
-    prefixes = []
-    for fname in mask_file_names:
-        prefixes.append(fname.split('mask')[0])
-    
-    return prefixes
+def get_image_fnames(mask_fnames):
+    image_fnames = [mask.replace('_mask', '') for mask in mask_fnames]
+    return image_fnames
 
-def load_images(prefixes):
-    for prefix in prefixes:
-        img_path = glob.glob
-masks = list(sorted(os.listdir("src/TrainingSrc/Data/PNGMasks")))
-images = list(sorted(os.listdir("src/TrainingSrc/Data/PNGImages")))
+def remove_unmatched_fnames(image_fnames, mask_fnames, all_image_fnames):
+    missing_images = list(set(image_fnames).difference(set(all_image_fnames)))
+    if len(missing_images)>0 :
+        print('File Name Error: files removed')
+        print(missing_images)
+        for m_image in missing_images:
+            image_fnames.remove(m_image)
+            mask_fnames.remove(m_image.replace('.', '_mask.'))
+    return image_fnames, mask_fnames
+
+mask_fnames = list(sorted(os.listdir("Data/PNGMasks")))
+image_fnames = get_image_fnames(mask_fnames)
+all_image_fnames = list(sorted(os.listdir("Data/PNGImages")))
+image_fnames, mask_fnames = remove_unmatched_fnames(image_fnames, mask_fnames, all_image_fnames)
+
 np_masks = []
 np_imgs = []
 rework_masks = []
 rework_masks_colors = []
 correct_unique_values = [0,64,127,255]
 
-for img in images:
-    np_imgs.append((np.array(Image.open(os.path.join("src/TrainingSrc/Data/PNGImages",img))), img))
+for img in image_fnames:
+    if not os.path.basename(img) == '.gitkeep' and not os.path.basename(img) == '.DS_Store':
+        np_imgs.append((np.array(Image.open(os.path.join("Data/PNGImages",img))), img))
 
-for mask in masks:
-    np_masks.append((np.array(Image.open(os.path.join("src/TrainingSrc/Data/PNGMasks", mask))), mask))
+for mask in mask_fnames:
+    if not os.path.basename(mask) == '.gitkeep' and not os.path.basename(mask) == '.DS_Store':
+        np_masks.append((np.array(Image.open(os.path.join("Data/PNGMasks", mask))), mask))
 
 for mask_d,img_d in zip(np_masks, np_imgs):
     # print(mask.shape)
