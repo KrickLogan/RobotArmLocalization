b81dc526b52e3628ce394c4d1d2b10416014f8e8
jacobdsiefkes <jacobdsiefkes@gmail.com>
2021-10-24 20:19:57 -0500

    Update test_run_script.py
    Center Point functions work now

--- src/RobotArmLocalizationPackage/test_run_script.py
+++ src/RobotArmLocalizationPackage/test_run_script.py
@@ -75,15 +75,39 @@ def show_img_mask(img, mask):
     maximize_plt()
     plt.show()
 
-def show_img_mask_center_point(img, mask, center_point):
+def show_img_mask_center_point(img, boxes):
+    center_point = get_center(boxes)
     plt.imshow(img)
-    plt.imshow(mask, cmap='ocean', alpha=.5)
     x = center_point[0]
     y = center_point[1]
+    x2 = center_point[2]
+    y2 = center_point[3]
+    x3 = center_point[4]
+    y3 = center_point[5]
     plt.plot(x, y, 'g*')
+    plt.plot(x2, y2, 'g*')
+    plt.plot(x3, y3, 'g*')
     plt.title('Center Point Predictions')
     maximize_plt()
     plt.show()
+
+def show_img_boxes(img, rectangles):
+    plt.imshow(img)
+    plt.title('Predicted Bounding Boxes')
+    for rect in rectangles:
+        plt.gca().add_patch(rect)
+
+    maximize_plt()
+    plt.show()
+
+def get_center(boxes):
+    points = []
+    for box in boxes:
+        x, y, w, h = box.detach().numpy()
+        p1 = (w + x)/2
+        p2 = (h + y)/2
+        points += [p1, p2]
+    return points
 
 def show_all_masks(img, masks):
     bool_masks = get_bool_masks(masks)
@@ -108,15 +132,6 @@ def show_mask_with_score_label_depth(img, bmask, score, label, depth, depth_arr)
     plt.imshow(bmask.numpy(), cmap='ocean', alpha=.5)
     plt.imshow(depth_arr, alpha=0)
     plt.title(f'Label: {get_label_string(label)}, Score: {round(score.item(),4)}, Average Depth: {round(depth, 4)}')
-    maximize_plt()
-    plt.show()
-
-def show_img_boxes(img, rectangles):
-    plt.imshow(img)
-    plt.title('Predicted Bounding Boxes')
-    for rect in rectangles:
-        plt.gca().add_patch(rect)
-
     maximize_plt()
     plt.show()
 
@@ -150,12 +165,14 @@ def main():
         masks = output[0]['masks']
         scores = output[0]['scores']
 
-        show_img(img)
+        #show_img(img)
 
         rectangles = get_rectangles(boxes)
-        show_img_boxes(img, rectangles)
+        #show_img_boxes(img, rectangles)
 
-        show_all_masks(img, masks)
+        #show_all_masks(img, masks)
+
+        show_img_mask_center_point(img, boxes)
 
         avg_depths = []
         bool_masks = get_bool_masks(masks)
@@ -166,11 +183,9 @@ def main():
         print(f'Scores: {scores}')
         print(f'Avg Depths: {avg_depths}')
 
-        for i in range(len(masks)):
+        #for i in range(len(masks)):
             #if scores[i] >= 0.90:
             #show_mask_with_score_label_depth(img, bool_masks[i], scores[i], labels[i], avg_depths[i], depth_arr)
-            show_img_mask_center_point(img, bool_masks[i], center_point)
-
 
 if __name__ == "__main__":
     main()
