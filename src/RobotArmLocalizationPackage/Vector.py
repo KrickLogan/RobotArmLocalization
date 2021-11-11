fbe6193ac1567b8e9702ac1ebe473a976f91dbd7
jacobdsiefkes <jacobdsiefkes@gmail.com>
2021-11-11 15:48:07 -0600

    Updated add, sub, mag, dot to no longer use np arrays

--- src/RobotArmLocalizationPackage/Vector.py
+++ src/RobotArmLocalizationPackage/Vector.py
@@ -3,34 +3,34 @@
     y is DEPTH, and z is vertical in 3d space relative to the camera.
 '''
 import numpy as np
-from math import cos, sin
+from math import cos, sin, sqrt
+from Vector import Vector
 
 class Vector:
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-    
 
-    def __add__(self, other):
-        a = np.array([self.x, self.y, self.z])
-        b = np.array([other.x, other.y, other.z])
-        return a + b
+    def __add__(self, other) -> Vector :
+        newx = self.x + other.x
+        newy = self.y + other.y
+        newz = self.z + other.z
+        return Vector(newx, newy, newz)
 
-    def __sub__(self, other):
-        a = np.array([self.x, self.y, self.z])
-        b = np.array([other.x, other.y, other.z])
-        return a - b
+    def __sub__(self, other) -> Vector :
+        newx = self.x - other.x
+        newy = self.y - other.y
+        newz = self.z - other.z
+        return Vector(newx, newy, newz)
 
-    def magnitude(self):
-        a = np.array([self.x, self.y, self.z])
-        mag = np.sqrt(a.dot(a))
+    def magnitude(self) -> float :
+        mag = sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
         return mag
 
-    def dot(self, other):
-        a = np.array([self.x, self.y, self.z])
-        b = np.array([other.x, other.y, other.z])
-        return np.dot(a, b)
+    def dot(self, other) -> float :
+        dot = self.x * other.x + self.y * other.y + self.z * other.z
+        return dot
 
    # def xy_rotate(self, angle):
         #a = np.array([self.x, self.y, self.z])
@@ -45,5 +45,6 @@ class Vector:
 
     def __str__(self) -> str:
         return(f"({self.x}, {self.y}, {self.z})")
+        
     def __repr__(self) -> str:
         return(f"({self.x}, {self.y}, {self.z})")
