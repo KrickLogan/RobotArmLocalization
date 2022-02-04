48c532ecf12b0d452c038abf058846b858c20c92
sushil khadka <58869935+skhadka4@users.noreply.github.com>
2022-02-04 12:45:39 -0700

    Update DetectedObject.py

--- src/RobotArmLocalizationPackage/DetectedObject.py
+++ src/RobotArmLocalizationPackage/DetectedObject.py
@@ -1,10 +1,12 @@
 from matplotlib.pyplot import axis
 import numpy as np
+from numpy.lib.function_base import average
 import torch
 import numpy.ma as ma
 import Utilities.utils as utils
-
+import sys
 class DetectedObject:
+    np.set_printoptions(threshold=sys.maxsize)
 
     def __init__(self, label, box, mask, score):
         self.label = label
@@ -30,6 +32,24 @@ class DetectedObject:
     def get_average_depth(self, depth_arr) -> float:
         bool_mask = self.get_bool_mask()
         bool_mask = np.logical_and(bool_mask, depth_arr != 0)
+        print(bool_mask)
         bool_mask = torch.gt(bool_mask, 0) #convert back to boolean
         mx = ma.masked_array(depth_arr, np.invert(bool_mask).long())
-        return mx.mean()
+        print(mx)
+        average_depth = self.remove_depth_outliers(mx)
+        return average_depth.mean()
+        
+    def remove_depth_outliers(self,depth_arr)->np.ndarray:
+        #removing outliers
+        
+        mean= np.mean(depth_arr)
+        std= np.std(depth_arr)
+        z= [x for x in depth_arr if(x>mean- 3* std)]
+        z= [x for x in depth_arr if(x < mean+ 3* std)]
+        print(mean,std)
+        print(z)
+        return z
+
+
+
+
