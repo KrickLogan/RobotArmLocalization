7f4aab325754ddf2fa05a6c420cfbc8ac1b06a0a
sushil khadka <58869935+skhadka4@users.noreply.github.com>
2022-02-07 15:09:28 -0700

    Update DetectedObject.py

--- src/RobotArmLocalizationPackage/DetectedObject.py
+++ src/RobotArmLocalizationPackage/DetectedObject.py
@@ -6,8 +6,6 @@ import numpy.ma as ma
 import Utilities.utils as utils
 import sys
 class DetectedObject:
-    np.set_printoptions(threshold=sys.maxsize)
-
     def __init__(self, label, box, mask, score):
         self.label = label
         self.box = box
@@ -22,7 +20,6 @@ class DetectedObject:
 
     def get_label(self):
         return self.label
-        
     def get_bool_mask(self) -> np.ndarray:   
         bool_mask = self.mask > utils.PRECISION
         # assert bool_mask.ndim == 2
@@ -32,22 +29,25 @@ class DetectedObject:
     def get_average_depth(self, depth_arr) -> float:
         bool_mask = self.get_bool_mask()
         bool_mask = np.logical_and(bool_mask, depth_arr != 0)
-        print(bool_mask)
-        bool_mask = torch.gt(bool_mask, 0) #convert back to boolean
-        mx = ma.masked_array(depth_arr, np.invert(bool_mask).long())
-        print(mx)
-        average_depth = self.remove_depth_outliers(mx)
-        return average_depth.mean()
+        bool_mask =depth_arr != 0
+        arr = depth_arr[bool_mask] 
+        average_depth = self.remove_depth_outliers(arr)
+
+        return average_depth.mean()  
         
     def remove_depth_outliers(self,depth_arr)->np.ndarray:
         #removing outliers
-        
         mean= np.mean(depth_arr)
         std= np.std(depth_arr)
-        z= [x for x in depth_arr if(x>mean- 3* std)]
-        z= [x for x in depth_arr if(x < mean+ 3* std)]
+        print("mean and std is:",mean,std)
+
+        flt = depth_arr.flatten()
+
+        z=flt[(flt>(mean- 3* std)) & (flt<(mean+3* std))]
+        print(" Result array : ", z)
+        mean = np.mean(z)
+        std= np.std(z)
         print(mean,std)
-        print(z)
         return z
 
 
