23c146e0998b7906f7adadeb7e8fa9440d845868
tdrps <70676631+tdrps@users.noreply.github.com>
2022-02-14 19:17:28 -0600

    add edited run2.py outside of package directory

--- run2.py
+++ run2.py
@@ -38,34 +38,56 @@ def main():
     img1 = get_image('frame_20')
     
     depth_arr = utils.load_depth_arr("frame_20_depth.npy")
-
+    print("calibration routine procedure:")
+    print("run detection on first image")
     detector = ObjectDetector(img1) # provide image to model
     
     detector.run() # Run model on image
-
+    viz.show_mask_overlay(img1, detector.get_claw().get_bool_mask(), "claw detection")
+    print("get relevant claw vectors")
     cam_claw_1 = detector.get_claw().to_vector(img1.size,depth_arr) - detector.get_base().to_vector(img1.size,depth_arr)
     pos_claw_1 = get_pos(cam_claw_1)
-
-    img2 = get_image('frame_25')
-    depth_arr2 = utils.load_depth_arr("frame_25_depth.npy")
+    print(f"camera's perspective, claw vector: {cam_claw_1}")
+    print(f"robot arm's perspective, claw vector: {pos_claw_1}")
+    
+    img2 = get_image('frame_112')
+    depth_arr2 = utils.load_depth_arr("frame_112_depth.npy")
 
     detector = ObjectDetector(img2) # provide image to model
     
     detector.run() # Run model on image
-
+    viz.show_mask_overlay(img2, detector.get_claw().get_bool_mask(), "claw detection")
     cam_claw_2 = detector.get_claw().to_vector(img2.size,depth_arr2) - detector.get_base().to_vector(img2.size,depth_arr2)
     pos_claw_2 = get_pos(cam_claw_2)
-
+    print(f"camera's perspective, claw vector: {cam_claw_2}")
+    print(f"robot arm's perspective, claw vector: {pos_claw_2}")
+    
     
     utils.calibrate(cam_claw_1, pos_claw_1, cam_claw_2, pos_claw_2)
+    print("save the rotation")
     l = Localizer()
     #Now we are ready to use the system.
 
-    target = detector.get_object().to_vector(img2.size,depth_arr2) - detector.get_base().to_vector(img2.size,depth_arr2)
+    print("take a new image with target in it and get the target vector")
+    img3 = get_image('frame_20')
+    depth_arr3 = utils.load_depth_arr("frame_20_depth.npy")
 
+    detector = ObjectDetector(img3) # provide image to model
+    
+    detector.run() # Run model on image
+    viz.show_mask_overlay(img3, detector.get_object().get_bool_mask(), "claw detection")
+    target = detector.get_object().to_vector(img3.size, depth_arr3) - detector.get_base().to_vector(img3.size, depth_arr3)
+    print(f"now get object vector from system: {target}")
+    print()
     calc_pos_vector = l.get_real_position(target)
-    print(calc_pos_vector)
-
+    print(f"apply the rotation to the target vector to get the position relative to the robot arm")
+    print()
+    print(f"the calculated position vector: {calc_pos_vector}")
+    input()
+    print()
+    print()
+    
+    print(f"Applying the same rotation in a test scenario, we show that this process does give the correct angle: {get_pos(target)}")
 
 if __name__ == "__main__":
     main()
