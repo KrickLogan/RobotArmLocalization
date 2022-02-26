d038a1d22bd2c3e1812e03aaff78f1fb77b190af
tdrps <70676631+tdrps@users.noreply.github.com>
2022-02-26 00:08:20 -0600

    demo docstrings and addition to conf.py file

--- src/arm_localizer/detected_object.py
+++ src/arm_localizer/detected_object.py
@@ -1,3 +1,4 @@
+import string
 from matplotlib.pyplot import axis
 import numpy as np
 from numpy.lib.function_base import average
@@ -7,23 +8,70 @@ from .utilities import utils
 from math import tan,radians
 from .vector import Vector
 
+
+
 class DetectedObject:
-    def __init__(self, label, box, mask, score):
+    '''A :class:`arm_localizer.detected_object.DetectedObject` is generated for each
+    detection from the :class:`arm_localizer.object_detector.ObjectDetector`.
+    This class stores the outputs from the RCNN model for a detection. The intention
+    of this class is to organize and label the model outputs for manipulation. Included
+    here are also relevant methods which use the model outputs.
+    '''
+    def __init__(self, label, box, mask, score, te):
+        '''Constructor Method
+        '''
+        self.te = te
         self.label = label
         self.box = box
         self.mask = mask
         self.score = score
     
     def get_center_pixel(self) -> tuple:
+        """This function returns the center pixel of the bounding box of a :class:`arm_localizer.detected_object.DetectedObject` as an (X,Y) tuple
+
+        This function uses the bounding box coordinates to calculate the (X,Y) coordinates of
+        the center point of the bounding box's pixel.
+
+        Args:
+        none
+
+        Returns:
+            tuple: (X,Y) coordinates of center pixel
+
+        """
         x1, y1, x2, y2 = self.box.detach().numpy()
         x = (x1 + x2)/2
         y = (y1 + y2)/2
         return((x,y))
 
     def get_label(self):
+        """Gets the label of the detected object
+
+    Each detection returned from the model includes a label. This method returns that label for this :class:`arm_localizer.detected_object.DetectedObject`
+
+    Args:
+        none
+
+    Returns:
+        string: label string
+
+    """
         return self.label
 
-    def get_bool_mask(self) -> np.ndarray:   
+    def get_bool_mask(self) -> np.ndarray:
+        """Returns a boolean array, true values representing a pixel including the detected object, false in pixels without a detected object
+
+        The RCNN Model returns an array called mask which contains values from 0 to 1 which are interpreted
+        here as the models certainty that the image contains 
+
+        Args:
+            arg1 (int): Description of arg1
+            arg2 (str): Description of arg2
+
+        Returns:
+            bool: Description of return value
+
+        """   
         bool_mask = self.mask > utils.PRECISION
         # assert bool_mask.ndim == 2
         bool_mask = np.squeeze(bool_mask)
--- src/docs/sphinx_notes/sphinx.md
+++ src/docs/sphinx_notes/sphinx.md
@@ -1,10 +1,26 @@
 # Sphinx notes
 
 - init files caused problems for the `sphinx-apidoc -o` command which generates rst files for each module. I just removed all the files I didn't want rst files for and ran it.
-- modify the `sphinx-apidoc -o` command to run on files from the utilities folder (./utilities/ eg)
+- modify the `sphinx-apidoc -o source ../arm_localizer` (directory arguments may be different. This particular command is relative to the docs directory) command to run on files from the utilities folder (./utilities/ eg)
 - in conf file, add the utilities folder to the path
 - haven't modified them to be classes yet
 - add the name of each rst file to the modules.rst file
 - Had to adjust the imports in each of the files which is creating a problem. Sphinx doesn't want: ` import arm_localizer.object_detector`, it will keep looking for the arm_localizer module. remove this. This does cause a problem for the package though. quick fix is to just remove the ```arm_localizer``` prefix, run the sphinx-autodoc command, and then replace them. This is something that should be solved using the init files inside the package.
+- When adding new docstrings or making a change to the code base which should be reflected in sphinx, you need to rebuild the backage, then run sphinx-apidoc -o(if necessary), then run `make clean html`
 
+## Docstring template for class member functions
 
+```
+"""Summary line.
+
+    Extended description of function.
+
+    Args:
+        arg1 (int): Description of arg1
+        arg2 (str): Description of arg2
+
+    Returns:
+        bool: Description of return value
+
+    """
+```
