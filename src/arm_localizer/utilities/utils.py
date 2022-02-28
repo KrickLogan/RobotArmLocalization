246ed8e688bd4b7f209cadeb824e5719bcb56e06
tdrps <70676631+tdrps@users.noreply.github.com>
2022-02-27 20:04:07 -0600

    docstrings for detected object

--- src/arm_localizer/utilities/utils.py
+++ src/arm_localizer/utilities/utils.py
@@ -21,7 +21,9 @@ HFOV = 69 #degrees
 VFOV = 42 #degrees
 
 def load_model():
-    # dirname = os.path.join(os.path.dirname(__file__),"../camera_data/depths/")
+
+    
+
     model = torch.load(os.path.join(os.path.dirname(__file__),'../data/model/model.pt'), map_location=torch.device('cpu'))
     return model
 
@@ -37,40 +39,11 @@ def load_depth_arr(dp_arr_name):
     np_depth = np.load(os.path.join(dirname, dp_arr_name))
     return np_depth
 
-# def get_center_point(box):
-#     x1, y1, x2, y2 = box.detach().numpy()
-#     x = (x1 + x2)/2
-#     y = (y1 + y2)/2
-#     return((x,y))
-
-# def get_bool_mask(mask):   
-#     bool_mask = mask > PRECISION 
-#     assert bool_mask.ndim == 2
-#     bool_mask = bool_mask.squeeze(1) #########Maybeeee???
-#     return bool_mask
-
-# def get_avg_depth(bool_mask, depth_arr):
-#     bool_mask = np.logical_and(bool_mask, depth_arr != 0)
-#     bool_mask = torch.gt(bool_mask, 0)
-#     mx = ma.masked_array(depth_arr, np.invert(bool_mask).long())
-#     return mx.mean()
-
 def get_vfov():
     return VFOV
 
 def get_hfov():
     return HFOV
-
-# def calculate_vector(box_center_pxl, img_center_pxl, depth):
-#     vertical_fov = get_vfov()
-#     horiz_fov = get_hfov()
-#     # z_angle = get_angle_between_pxls(box_center_pxl[1], img_center_pxl[1], vertical_fov) #angle in zy plane-angle of elevation
-#     # x_angle = get_angle_between_pxls(box_center_pxl[0], img_center_pxl[0], horiz_fov) #Angle in xy plane
-#     x_angle, z_angle = get_angles_between_pixels(box_center_pxl, img_center_pxl, vertical_fov, horiz_fov)
-#     z = get_coord_value(z_angle, depth)
-#     x = get_coord_value(x_angle, depth)
-#     y = depth
-#     return (x,y,z)
 
 def get_label_string(label): ## Rework to use a dict?
     label_string = ''
