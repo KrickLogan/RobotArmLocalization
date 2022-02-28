f4e093598193e7856db77f3b8b4ced8837645a8f
yadav-95 <Yadav.bhetuwal@gmail.com>
2022-02-28 11:58:00 -0600

    Update localizer.py

--- src/arm_localizer/localizer.py
+++ src/arm_localizer/localizer.py
@@ -1,8 +1,8 @@
+from fileinput import filename
+from logging import FileHandler
 from math import tan,radians
 import pickle
-import traceback
-import zlib
-# from typing import List
+from typing import List
 
 from PIL.Image import Image
 from arm_localizer.detected_object import DetectedObject
@@ -22,31 +22,32 @@ class LocalizerNotInitializedError(Exception):
 class Localizer:
     
     def __init__(self):
-        # self.f_rot_vector = None
-        # self.f_rot_rads = None
-        # self.s_rot_vector = None
-        # self.s_rot_rads = None
+        self.f_rot_vector = None
+        self.f_rot_rads = None
+        self.s_rot_vector = None
+        self.s_rot_rads = None
         # self.rotation = utils.unpickle()  
-        filename = "./rotation/rotation.pickle"
+        filename = "./rotation.pkl"
         fh = open(filename, "rb")
         try:
             fh_new = pickle.load(fh)
         except pickle.UnpicklingError as e:
-            print(traceback.format_exc(e))
+            print(e)
             raise LocalizerNotInitializedError(f'Unable to load rotation. Need to load rotation to initialize package{filename}.')
         except pickle.PicklingError as e:
-            print(traceback.format_exc(e))
+            print(e)
             raise LocalizerNotInitializedError(f'Unable to load rotation. Need to load rotation to initialize package{filename}.')
         except (AttributeError,  EOFError, ImportError, IndexError) as e:
-            print(traceback.format_exc(e))
+            print(e)
             raise LocalizerNotInitializedError(f'Unable to load rotation. Need to load rotation to initialize package{filename}.')
         except Exception as e:
-            print(traceback.format_exc(e))
+            print(e)
             raise LocalizerNotInitializedError(f'Unable to load rotation. Need to load rotation to initialize package{filename}.')
         else:
             self.rotation = fh_new
         finally:
             fh.close()
+
         
     def get_real_position(self, t_vector: Vector) -> Vector:
         ''' This Function is the final usage of the system. It applies the rotations to the vector of the detected object
@@ -56,10 +57,10 @@ class Localizer:
         t_vector=t_vector.rotate_about_vector(self.rotation._s_rot_vector, self.rotation._s_rot_rads)
         return t_vector
     
-    # def calibrate(self, cam_claw_1: Vector, pos_claw_1: Vector, cam_claw_2: Vector, pos_claw_2: Vector):
-    #     self.f_rot_vector = cam_claw_1.cross(pos_claw_1)
-    #     self.f_rot_rads = cam_claw_1.angle_between(pos_claw_1)
-    #     self.s_rot_vector = pos_claw_1
-    #     cam_claw_2 = cam_claw_2.rotate_about_vector(self.f_rot_vector.unit(),self.f_rot_rads)
-    #     self.s_rot_rads = cam_claw_2.perp(self.s_rot_vector).angle_between(pos_claw_2.perp(self.s_rot_vector))
+    def calibrate(self, cam_claw_1: Vector, pos_claw_1: Vector, cam_claw_2: Vector, pos_claw_2: Vector):
+        self.f_rot_vector = cam_claw_1.cross(pos_claw_1)
+        self.f_rot_rads = cam_claw_1.angle_between(pos_claw_1)
+        self.s_rot_vector = pos_claw_1
+        cam_claw_2 = cam_claw_2.rotate_about_vector(self.f_rot_vector.unit(),self.f_rot_rads)
+        self.s_rot_rads = cam_claw_2.perp(self.s_rot_vector).angle_between(pos_claw_2.perp(self.s_rot_vector))
         
