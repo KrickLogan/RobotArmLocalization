3e03a75d6431108bed53fc1795b36a6d9598ad86
jserf22 <jserf22@live.com>
2022-04-15 19:53:37 -0500

    created both demo scripts for calibration and localizer

--- src/arm_localizer/demo_scripts/demo_calibration.py
+++ src/arm_localizer/demo_scripts/demo_calibration.py
@@ -3,179 +3,38 @@ from arm_localizer import *
 from arm_localizer import visualizer as viz
 from math import radians, degrees
 from PIL import Image
-import os
-
-    
-def get_center_points(detector):
-    """Gets a list of center points
-
-    This function returns all of the center points from the ObjectDetector in the form of a list.
-    1. Obtain the list of DetectedObjects from the model outputs, instances of classifications
-    2. Initialize p with a value of the untransformed pixel value coordinates representing DetectedObjects calculated center of mass
-    3. Add center of mass point to the list of points    
-
-    Args:
-        detector (ObjectDetector): returns the model detections 
-    
-    returns:
-        points: Returns a list of the points representing the DetectedObjects center of mass
-
-    """
-    points = []    
-    for d in detector.get_all_detections():        
-        p = d.get_center_mass_pixel()        
-        points.append(p)   
-    return points
-
-def show_all_detections(img, depth_arr, detector):
-    """Show the model detections and mask overlaid
-
-    This function obtains all classifications and shows the detections with the mask overlays. 
-    Display the claw.
-    Display the base.
-    Display the object.
-    gets bounding boxes
-    gets center points
-
-    Args:
-        img (image): rgb data
-        depth_arr (np.array): depth array, depths in mm
-        detector (ObjectDetector): Provides output from model, classifications
-
-    Returns:
-        none
-
-    """
-    # for d in detector.get_all_detections():
-    #     viz.show_mask_overlay(img, d.get_bool_mask(), "detection")
-    #     viz.show_mask_overlay(depth_arr, d.get_bool_mask(), "detection", force_contrast=True, depth_arr=depth_arr)
-    
-    viz.show_mask_overlay(img, detector.get_claw().get_bool_mask(), "claw detection")
-    viz.show_mask_overlay(img, detector.get_base().get_bool_mask(), "Base Detection")
-    viz.show_mask_overlay(img, detector.get_object().get_bool_mask(), "Object Detection")
-    
-    # viz.show_mask_overlay(depth_arr, detector.get_claw().get_bool_mask(), "claw detection", force_contrast=True, depth_arr=depth_arr)
-    # viz.show_mask_overlay(depth_arr, detector.get_base().get_bool_mask(), "Base Detection", force_contrast=True, depth_arr=depth_arr)
-    # viz.show_mask_overlay(depth_arr, detector.get_object().get_bool_mask(), "Object Detection", force_contrast=True, depth_arr=depth_arr)
-
-
-    rectangles = viz.get_rectangles([detector.get_claw().box, detector.get_base().box, detector.get_object().box])
-    viz.show_img_boxes(img, rectangles)
-
-    center_points = get_center_points(detector)
-    # print(center_points)
-    viz.show_points(img, center_points, "Center Points")
-
-def show_all_vectors(cam_claw_1, cam_claw_2, pos_claw_1, pos_claw_2, cam_obj, pos_obj, og, norm=None):
-    """_summary_
-
-    Args:
-        cam_claw_1 (_type_): The position of the claw from the camera 
-        cam_claw_2 (_type_): _description_
-        pos_claw_1 (Vector): The position of the claw from the Robot arm coordinate system which corresponds to the first depth and img
-        pos_claw_2 (Vector): The position of the claw from the Robot arm coordinate system which corresponds to the second depth and img
-        cam_obj (_type_): _description_
-        pos_obj (_type_): _description_
-        og (Vector): The zero vector originating at the origin
-        norm (_type_, optional): _description_. Defaults to None.
-    """
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    x,y,z = og.as_point()
-
-    for pt,col in zip([cam_obj, pos_obj],['y','g']):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color=col)
-        ax.scatter(a,b,c,marker="^", color='k')
-
-    for pt,col in zip([cam_claw_1, cam_claw_2],['c','m']):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='b')
-        ax.scatter(a,b,c,marker="^", color=col)
-
-    for pt,col in zip([pos_claw_1,pos_claw_2],["c","m"]):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='r')
-        ax.scatter(a,b,c,marker="^", color=col)
-
-    if norm != None:
-        a,b,c = norm.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='k')
-        ax.scatter(a,b,c,marker="^", color='k')
-    
-    ax.scatter(x,y,z, marker="o")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    ax.set_xlim([-500,500])
-    ax.set_ylim([-500,500])
-    ax.set_zlim([-200,500])
-    plt.show()
+import os    
 
 def main():
-    """Carries out the execution of the calibration functions
-
-    The main function steps through the individual steps of the calibration process.
-    1. loads first and second images: each are done individually and are corresponding to a separate claw position
-    2. loads first and second depth arrays: each are done individually and are corresponding to a separate claw position
-    3. get actual claw position from positioning system of the robot arm
-    4. 
-
-    This function 
     
-    """
+    # Put your path to the first image here
+    img1_filename = "./camera_data/images/3_15_22/position_1/claw_1.png"
+    # Put your path to the second image here
+    img2_filename = "./camera_data/images/3_15_22/position_1/claw_2.png"
 
-    # initialize origin with a vector of zero's
-    og = Vector(0,0,0)
+    # Put your path to the first depth here 
+    depth1_filename = "./camera_data/depths/3_15_22/position_1/claw_1_depth.npy"
+    # Put your path to the second depth here
+    depth2_filename = "./camera_data/depths/3_15_22/position_1/claw_2_depth.npy"
 
-    # The position of claws initialized with initial vector poisitions (claw to base) in mm
-    pos_claw_1 = Vector(247, 110, 211)
+    # Load the first image and open as an RGB image, load the first depth array (measured in mm)
+    img1 = Image.open(os.path.join(img1_filename)).convert("RGB") 
+    depth_arr1 = np.load(os.path.join(depth1_filename))  
+
+    # Put in the measure position of the claw from the robot arm or get the postition of the claw from the robot arm
+    pos_claw_1 = Vector(247, 110, 211) 
+
+    # Load the second image and open as an RGB image, load the second depth array (measured in mm). 
+    img2 = Image.open(os.path.join(img2_filename)).convert("RGB")    
+    depth_arr2 = np.load(os.path.join(depth2_filename))
+
+    # Put in the measure position of the claw from the robot arm or get the postition of the claw from the robot arm
     pos_claw_2 = Vector(268, -90, 173)
 
-    # This initializes pos_obj with a vector. Object measured from base in mm.  
-    pos_obj = Vector(439, -202, -22)
-    # This initializes pos_obj2 with vector of object
-    pos_obj2 = Vector(245, 202, -22)
-    pos_obj3 = Vector(459, 78, 205)
-
-    # Camera position is initialized as the first postition, postion 1
-    camera_position = "position_1"
-    # camera_position = 'position_2'
-    # camera_position = 'position_3'
-    # camera_position = 'position_3b'
-    # camera_position = 'position_4'
-    # camera_position = 'position_5'
-    # camera_position = 'position_6'
+    # calculates a transformation for converting positions between the camera coordinate system and the robot arm system. 
+    # Important: Needs to have both sets of images, depths and claws to complete this rotation.
+    # The end result is a new rotation folder and file saved
+    rs_calibrate(img1, depth_arr1, img2, depth_arr2, pos_claw_1, pos_claw_2)    
     
-    # create file_names that contains a list with claw and object values
-    file_names=['claw_1', 'claw_2', 'obj_2', 'obj_3']
-    frame_prefix = file_names[0]
-
-    img1 = Image.open(os.path.join("camera_data","images", '3_15_22', camera_position, f"{frame_prefix}.png")).convert("RGB")
-
-    # depth_arr1 = utils.load_depth_arr(f"./camera_data/depths/{align_to}/{camera_position}/{frame_prefix}_depth.npy")
-    depth_arr1 = np.load(os.path.join("camera_data", "depths", "3_15_22", camera_position, f"{frame_prefix}_depth.npy"))
-    
-    plt.imshow(img1)
-    plt.show()
-
-    frame_prefix = file_names[1]
-    img2 = Image.open(os.path.join("camera_data","images", '3_15_22', camera_position, f"{frame_prefix}.png")).convert("RGB")
-
-    # depth_arr1 = utils.load_depth_arr(f"./camera_data/depths/{align_to}/{camera_position}/{frame_prefix}_depth.npy")
-    depth_arr2 = np.load(os.path.join("camera_data", "depths", "3_15_22", camera_position, f"{frame_prefix}_depth.npy"))
-    
-    plt.imshow(img2)
-    plt.show()
-
-    rs_calibrate(img1, depth_arr1, img2, depth_arr2, pos_claw_1, pos_claw_2)
-
-    detector = ObjectDetector()
-
-    detector.run(img2)
-
-    show_all_detections(img2, depth_arr2, detector)
-    
-
 if __name__ == "__main__":
     main()
--- src/arm_localizer/demo_scripts/demo_localizer.py
+++ src/arm_localizer/demo_scripts/demo_localizer.py
@@ -6,130 +6,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import degrees
 
-def get_center_points(detector):
-    points = []
-    dets = [detector.get_claw(), detector.get_object(), detector.get_base()]
-    for d in dets:
-        p = d.get_center_mass_pixel()
-        points.append(p)
-    return points
+def main(): 
 
+    # Put your path to the image here
+    img_filename = "./camera_data/images/3_15_22/position_1/claw_1.png"    
 
-def show_all_detections(img, depth_arr, detector):
-    # for d in detector.get_all_detections():
-    #     viz.show_mask_overlay(img, d.get_bool_mask(), "detection")
-    #     viz.show_mask_overlay(depth_arr, d.get_bool_mask(), "detection", force_contrast=True, depth_arr=depth_arr)
+    # Put your path to the depth here
+    depth_filename = "./camera_data/depths/3_15_22/position_1/claw_1_depth.npy"
+
+    # Load the first image and open as an RGB image, load the first depth array (measured in mm) 
+    img = Image.open(os.path.join(img_filename)).convert("RGB") 
+
+    depth_arr = np.load(os.path.join(depth_filename)) 
     
-    viz.show_mask_overlay(img, detector.get_claw().get_bool_mask(), "claw detection")
-    viz.show_mask_overlay(img, detector.get_base().get_bool_mask(), "Base Detection")
-    viz.show_mask_overlay(img, detector.get_object().get_bool_mask(), "Object Detection")
-    
-    viz.show_mask_overlay(depth_arr, detector.get_claw().get_bool_mask(), "claw detection", force_contrast=True, depth_arr=depth_arr)
-    viz.show_mask_overlay(depth_arr, detector.get_base().get_bool_mask(), "Base Detection", force_contrast=True, depth_arr=depth_arr)
-    viz.show_mask_overlay(depth_arr, detector.get_object().get_bool_mask(), "Object Detection", force_contrast=True, depth_arr=depth_arr)
-
-    rectangles = viz.get_rectangles([detector.get_claw().box, detector.get_base().box, detector.get_object().box])
-    viz.show_img_boxes(img, rectangles)
-
-    center_points = get_center_points(detector)
-    # print(center_points)
-    viz.show_points(img, center_points, "Center Points")
-
-def show_all_vectors(cam_claw_1, cam_claw_2, pos_claw_1, pos_claw_2, cam_obj, pos_obj, og, norm=None):
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    x,y,z = og.as_point()
-
-    for pt,col in zip([cam_obj, pos_obj],['y','g']):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color=col)
-        ax.scatter(a,b,c,marker="^", color='k')
-
-    for pt,col in zip([cam_claw_1, cam_claw_2],['c','m']):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='b')
-        ax.scatter(a,b,c,marker="^", color=col)
-
-    for pt,col in zip([pos_claw_1,pos_claw_2],["c","m"]):
-        a,b,c = pt.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='r')
-        ax.scatter(a,b,c,marker="^", color=col)
-
-    if norm != None:
-        a,b,c = norm.as_point()
-        ax.plot([x,a],[y,b],[z,c], color='k')
-        ax.scatter(a,b,c,marker="^", color='k')
-    
-    ax.scatter(x,y,z, marker="o")
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    ax.set_xlim([-500,500])
-    ax.set_ylim([-500,500])
-    ax.set_zlim([-200,500])
-    plt.show()
-
-def main():
-    og = Vector(0,0,0)
-
-    pos_claw_1 = Vector(247, 110, 211)
-    pos_claw_2 = Vector(268, -90, 173)
-
-    pos_obj1 = Vector(439, -202, -22)
-    pos_obj2 = Vector(245, 202, -22)
-    pos_obj3 = Vector(459, 78, 205)
-
-    object_positions = [pos_obj1, pos_obj2, pos_obj3]
-
-    
-    camera_position = "position_1"
-    # camera_position = 'position_2'
-    # camera_position = 'position_3'
-    # camera_position = 'position_3b'
-    # camera_position = 'position_4'
-    # camera_position = 'position_5'
-    # camera_position = 'position_6'
-    
-    file_names=['claw_2', 'obj_2', 'obj_3']
-    obj_position = 0
-    frame_prefix = file_names[obj_position]
-    object_positions = [pos_obj1, pos_obj2, pos_obj3]
-    pos_obj = object_positions[obj_position]
-
-    img = Image.open(os.path.join("camera_data","images", '3_15_22', camera_position, f"{frame_prefix}.png")).convert("RGB")
-
-    # depth_arr1 = utils.load_depth_arr(f"./camera_data/depths/{align_to}/{camera_position}/{frame_prefix}_depth.npy")
-    depth_arr = np.load(os.path.join("camera_data", "depths", "3_15_22", camera_position, f"{frame_prefix}_depth.npy"))
-    
-    plt.imshow(img)
-    plt.show()
-
+    # initiates detector as the pointer to the detections (output) from the model
     detector = ObjectDetector()
 
+    # run the second image on the ObjectDetector to get the detections from the model
     detector.run(img)
 
-    obj_vector = detector.get_object().rs_to_vector( depth_arr) - detector.get_base().rs_to_vector( depth_arr)
-    show_all_vectors(cam_claw_1=Vector(0,0,0), cam_claw_2=Vector(0,0,0), pos_claw_1=pos_claw_1, pos_claw_2=pos_claw_2, cam_obj=obj_vector, pos_obj=pos_obj, og=og)
+    # Initializes the obj_vector that stores the detected objects converted position vector
+    obj_vector = detector.get_object().rs_to_vector(depth_arr) - detector.get_base().rs_to_vector(depth_arr)
+   
+    # initialize l to point to the localizer function
     l = Localizer()
 
-    calculated_position = l.get_real_position(obj_vector)
-
-    # calculated_position = locate_object2(img, depth_arr)
-
-    show_all_vectors(cam_claw_1=Vector(0,0,0), cam_claw_2=Vector(0,0,0), pos_claw_1=pos_claw_1, pos_claw_2=pos_claw_2, cam_obj=calculated_position, pos_obj=pos_obj, og=og)
-
-    if input("show detections? ") != 'n':
-        show_all_detections(img, depth_arr, detector)
-
-    print("\n\nAll vector coordinates and measurements are in millimeters")
-    print(f"Calculated Position Vector: {calculated_position}\nActual Position Vector: {pos_obj}\n")
-    cmag = round(calculated_position.magnitude())
-    pmag = round(pos_obj.magnitude())
-    print(f"Magnitudes\nCalculation: {cmag}\nActual: {pmag}\n")
-    print(f"Angle Between: {round(degrees(calculated_position.angle_between(pos_obj)))}\n")
-    e = pos_obj - calculated_position
-    print(f"Error Vector: {e}, Magnitude: {round(e.magnitude())}")
-    print(f"Net Magnitude error: {abs(round(e.magnitude()) - abs(pmag - cmag))}")
+    # stores the true position of the detected object after conversion. You need to have both saved rotations from the demo_calibration in 
+    # order for get_real_position to return the true position after calculation. The localizer calls this function to get the result that we want.
+    calculated_position = l.get_real_position(obj_vector)  
+    print (f"calculated position: {calculated_position}")    
 
 if __name__ == "__main__":
     main()
